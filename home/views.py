1d09c931c5fb199f7e106d9758cb1e0d60965b4f
madhuReddyDasari <dasarilovamadhu@gmail.com>
2023-11-28 02:00:50 +0530

    Added basic template to render for each view

--- home/views.py
+++ home/views.py
@@ -4,16 +4,20 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return HttpResponse("This is a home page")
+    # return HttpResponse("This is a home page")
+    return render(request, 'home.html')
 
 
 def about(request):
-    return HttpResponse("This is a about page")
+    # return HttpResponse("This is a about page")
+    return render(request, 'about.html')
 
 
 def contact(request):
-    return HttpResponse("This is a contact page")
+    # return HttpResponse("This is a contact page")
+    return render(request, 'contact.html')
 
 
 def projects(request):
-    return HttpResponse("This is a projects page")
+    # return HttpResponse("This is a projects page")
+    return render(request, 'projects.html')
